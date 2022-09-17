b088e22f8a5e8e5eb0bcd87346e7e2cdea9804bc
Владислав Антонов <lolik1806@gmail.com>
2022-09-17 10:42:38 +0300

    add handler for create graph

--- internal/graph/handler.go
+++ internal/graph/handler.go
@@ -2,6 +2,8 @@ package graph
 
 import (
 	"GraphsApi/internal/handlers"
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -14,11 +16,25 @@ func NewHandler() handlers.Handler {
 }
 
 func (h *handler) Register(router *mux.Router) {
-	router.HandleFunc("/graph", h.Create()).Methods(http.MethodPost)
+	router.HandleFunc("/create", h.Create()).Methods(http.MethodPost)
 }
 
 func (h *handler) Create() http.HandlerFunc {
-	return func(response http.ResponseWriter, request *http.Request) {
+	var requestData struct {
+		Matrix               [][]int `json:"matrix"`
+		Start                int     `json:"start"`
+		End                  int     `json:"end"`
+		VertexCount          int     `json:"vertexCount"`
+		ShowIntermediateInfo bool    `json:"showIntermediateInfo"`
+	}
 
+	return func(response http.ResponseWriter, request *http.Request) {
+		err := json.NewDecoder(request.Body).Decode(&requestData)
+		if err != nil {
+			http.Error(response, err.Error(), http.StatusBadRequest)
+		}
+
+		response.WriteHeader(200)
+		fmt.Println(requestData)
 	}
 }
